cbf4abea51ef1ca562a0c58f7256a0fedc5a58a2
Amit Umbare <amitprashantumbare@gmail.com>
2026-03-12 19:03:51 +0530

    Rename DemoApplication to TradingPlatformApplication and finalize Dockerized backend

--- src/main/java/com/example/tradingplatform/TradingPlatformApplication.java
+++ src/main/java/com/example/tradingplatform/TradingPlatformApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cache.annotation.EnableCaching;
 @EnableCaching
 @SpringBootApplication
 public class
-DemoApplication {
+TradingPlatformApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(TradingPlatformApplication.class, args);
 	}
 
 }
