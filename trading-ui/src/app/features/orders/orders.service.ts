4fd85c80cc7d3af21a5a1e315a4f864e02123d20
Amit Umbare <amitprashantumbare@gmail.com>
2026-03-16 19:29:03 +0530

    fix: correct environment import paths

--- trading-ui/src/app/features/orders/orders.service.ts
+++ trading-ui/src/app/features/orders/orders.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { InstrumentService } from '../../core/instrument.service';
-import { environment } from '../../environments/environment';
+import { environment } from '../../../environments/environment';
 
 export interface OrderResponse {
 
